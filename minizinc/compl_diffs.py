56b66bc98f8468d9ece24c642c162cb85a329ee1
Nyriu <muninitristano@gmail.com>
2020-04-17 00:22:49 +0200

    bar plot per confronti

--- minizinc/compl_diffs.py
+++ minizinc/compl_diffs.py
@@ -1,11 +1,10 @@
-# TODO
 # Data una coppia K,H mostra i valori MPOQ delle istanze
 # - risolte in minor tempo
 # - risolte in maggior tempo
 # - con minor numero di persone
 # - con maggior numero di persone
 # Se un istanza è candidata per più tipologie viene mostrata una sola volta
-# Le istanze sono ordinate da sinistra a destra per solveTime crescente
+# TODO Le istanze sono ordinate da sinistra a destra per solveTime crescente
 
 import my_lib
 import os
@@ -17,40 +16,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-deep')
 
+keys = ['M','P','O','Q']
+
+K = 2
+H = 3
+
+
 inputs = []
 solveTimes = []
 nums = []
 
-
+# Raccolgo i dati che devo plottare
 num = 0
-i = my_lib.get_input(num)
-d = my_lib.read_output(num, my_lib.OUTPUT_DIR, suppress_error=True)
-
-while not (d == None):
-    inputs.append(i)
-    solveTimes.append(d['stats']['solveTime'])
-    nums.append(num)
+inp = my_lib.get_input(num)
+out = my_lib.read_output(num, my_lib.OUTPUT_DIR, suppress_error=True)
+while not (out == None):
+    if inp['K'] == K and inp['H'] == H:
+        inputs.append(inp)
+        solveTimes.append(out['stats']['solveTime'])
+        nums.append(num)
 
     num+=1
-    i = my_lib.get_input(num)
-    d = my_lib.read_output(num, my_lib.OUTPUT_DIR, suppress_error=True)
+    inp = my_lib.get_input(num)
+    out = my_lib.read_output(num, my_lib.OUTPUT_DIR, suppress_error=True)
 
-keys = ['M','P','O','Q']
 
-cut_l = 5
-nums = nums[:cut_l]
-solveTimes = solveTimes[:cut_l]
-inputs = inputs[:cut_l]
+## DEBUG STUFF
+#cut_l = 5
+#nums = nums[:cut_l]
+#solveTimes = solveTimes[:cut_l]
+#inputs = inputs[:cut_l]
+#
+#for i in inputs:
+#    print(i)
+##END DEBUG STUFF
 
-for i in inputs:
-    print(i)
+# Selezioni le istanze che poi plottero'
+# Mantengo gli indici relativi
+ind_min_sTime = None # risolto in meno tempo
+ind_max_sTime = None # risolto in piu' tempo
+ind_min_peps  = None  # con minor numero di persone
+ind_max_peps  = None  # con maggior numero di persone
+for i, inp in enumerate(inputs):
+    if ind_min_sTime == None or solveTimes[i] < solveTimes[ind_min_sTime]:
+        ind_min_sTime = i
+    elif ind_max_sTime == None or solveTimes[i] > solveTimes[ind_max_sTime]:
+        ind_max_sTime = i
 
-m = [i['M'] for i in inputs]
-p = [i['P'] for i in inputs]
-o = [i['O'] for i in inputs]
-q = [i['Q'] for i in inputs]
+    if ind_min_peps == None and ind_max_peps == None:
+        ind_min_peps = i
+        ind_max_peps = i
 
-labels = [str(n) for n in nums]
+    m_peps = 0 # min num persone
+    M_peps = 0 # max num persone
+    peps = 0   # persone correnti
+    for k in keys:
+        m_peps += inputs[ind_min_peps][k]
+        M_peps += inputs[ind_max_peps][k]
+        peps += inp[k]
+    if peps < m_peps:
+        ind_min_peps = i
+    elif peps > M_peps:
+        ind_max_peps = i
+
+
+# Rimuovo duplicati
+selected = list(set([ind_min_sTime, ind_max_sTime, ind_min_peps, ind_max_peps]))
+
+s_inputs     = [inputs[i] for i in selected]
+s_solveTimes = [solveTimes[i] for i in selected]
+s_nums       = [nums[i] for i in selected]
+
+
+m = [i['M'] for i in s_inputs]
+p = [i['P'] for i in s_inputs]
+o = [i['O'] for i in s_inputs]
+q = [i['Q'] for i in s_inputs]
+
+labels = [str(n) for n in s_nums]
 
 x = np.arange(len(labels))  # the label locations
 width = 0.1  # the width of the bars
@@ -80,6 +123,21 @@ def autolabel(rects):
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
                     ha='center', va='bottom')
+
+# Annoto i tempi
+plt.annotate('{}'.format("solveTimes"), TODO
+            xy=(-1, 0),
+            xytext=(0, 3),  # 3 points vertical offset
+            textcoords="offset points",
+            #TODO background color?
+            ha='center', va='bottom')
+for pos in x:
+    ax.annotate('{}'.format(s_solveTimes[pos]),
+                xy=(pos, 0),
+                xytext=(0, 3),  # 3 points vertical offset
+                textcoords="offset points",
+                #TODO background color?
+                ha='center', va='bottom')
 
 autolabel(rects_m)
 autolabel(rects_p)
