192d22cc88fcb6aae16f1b38d819782b40073564
Nyriu <muninitristano@gmail.com>
2020-04-18 16:59:59 +0200

    Test su solver diversi

---
+++ minizinc/tmp_on_solvers/gecode/in_out_visualizer.py
@@ -0,0 +1,208 @@
+#### TODO
+#### Esegue il modello MiniZinc su tutti gli input
+#### Per ciascuno mostra input e infomrazioni sulla soluzione trovata
+#### con le frecce sx dx si passa all'input precedente o successivo
+#### TODO # con le frecce su giu' si passa alla soluzione precedente o successiva
+###
+#### Usage:
+#### python in_out_visualizer.py
+###
+####import sys
+###import my_lib
+###
+###def get_args():
+###    args = sys.argv
+###    if len(args) < 2:
+###        print("Put pone arguments!! For example")
+###        print("python run.py 10")
+###        exit(1)
+###
+###    try:
+###        input_num = int(args[1])
+###    except:
+###        print("ERROR! Input num is not ad int!")
+###        exit(2)
+###    return input_num
+###
+###
+###
+###
+#####################################################
+#### Main
+#####################################################
+###def main():
+###    input_num = get_args()
+###    #my_lib.run_minizinc_model(input_num, show_output=True)
+###    my_lib.run_minizinc_model(input_num)
+###
+###
+###if __name__ == "__main__":
+###    main()
+###
+import my_lib
+
+import curses
+from curses import textpad, wrapper
+import lipsum
+
+
+#EXIT_KEYS = [ curses.KEY_EXIT , 'q', 'Q' ]
+#NEXT_KEYS = [ curses.KEY_RIGHT, 'l', 'L' ]
+#PREV_KEYS = [ curses.KEY_LEFT , 'h', 'H' ]
+
+EXIT_KEYS = [ curses.KEY_EXIT , ord('q'), ord('Q') ]
+NEXT_KEYS = [ curses.KEY_RIGHT, ord('l'), ord('L') ]
+PREV_KEYS = [ curses.KEY_LEFT , ord('h'), ord('H') ]
+
+def border(win, xy_offset=0, dim_offset=0):
+    height,width = win.getmaxyx()
+
+    uly = 1 + xy_offset
+    ulx = 1 + xy_offset
+    lry = height -1 - dim_offset
+    lrx = width  -2 - dim_offset
+
+    textpad.rectangle(win, uly, ulx, lry, lrx)
+
+
+def init_wins():
+    btw_gap = 1
+    below_gap = 3
+    # bottom win
+    bh = 4
+    bw = curses.COLS -2
+    b_uly = curses.LINES - below_gap - bh
+    b_ulx = 2
+    # left win
+    l_uly = 2
+    l_ulx = 2
+    lh = curses.LINES - l_uly - below_gap - bh
+    lw = curses.COLS//2
+    # right win
+    r_uly = l_uly
+    r_ulx = lw + btw_gap
+    rw = lw - btw_gap - l_ulx
+    rh = lh
+
+    l_win = curses.newwin(lh, lw, l_uly, l_ulx)
+    r_win = curses.newwin(rh, rw, r_uly, r_ulx)
+    b_win = curses.newwin(bh, bw, b_uly, b_ulx)
+
+    for w in [l_win, r_win, b_win]:
+        w.keypad(True)
+        w.clear()
+
+    l_win.border(0)
+
+    # left win text
+    lt_begin_y = 2
+    lt_begin_x = 2
+    # right win text
+    rt_begin_y = lt_begin_y
+    rt_begin_x = lt_begin_x
+    # right win text
+    bt_begin_y = 2
+    bt_begin_x = 2
+
+    #text = lipsum.generate_paragraphs(1)
+    text = "Test text"
+    l_win.addstr(lt_begin_y, lt_begin_x, text)
+    r_win.addstr(rt_begin_y, rt_begin_x, text)
+    b_win.addstr(bt_begin_y, bt_begin_x, text)
+    #border(l_win)
+    #border(r_win)
+    border(b_win)
+
+    return l_win, r_win, b_win
+
+
+def main(stdscr):
+    # Init
+    curses.curs_set(False)
+    curses.cbreak()
+    curses.noecho()
+    stdscr.keypad(True)
+    stdscr.clear()
+    print("DEBUG", file=open('tmp.txt', 'w+')) #DEBUG
+
+    try:
+        l_win, r_win, b_win = init_wins()
+        #for w in [l_win, r_win, b_win]:
+        #    text = "Press a button"
+        #    w.clear()
+        #    w.addstr(0,0, text)
+        #    w.refresh()
+
+        num = 0
+
+        #c = b_win.getkey()
+        #c = b_win.getch()
+        #c = stdscr.getch()
+        c = 'a' # uno a caso
+        input_text = "Press a button"
+        output = "Press a button"
+
+        print("c=a", file=open('tmp.txt', 'a+'))
+
+
+        while not c in EXIT_KEYS:
+            old_num = num
+            if c in NEXT_KEYS:
+                num +=1
+            if c in PREV_KEYS and num > 0:
+                num -=1
+
+            try:
+                old_input_text = input_text
+                old_output = output
+
+                input_text = my_lib.get_input_text(num)
+                output = my_lib.get_output(num)
+                print("in fondo al try", file=open('tmp.txt', 'a+'))
+            except:
+                input_text = old_input_text
+                output = old_output
+                num = old_num
+                print("in fondo al except", file=open('tmp.txt', 'a+'))
+
+            print(input_text, file=open('tmp.txt', 'a+'))
+            print(output, file=open('tmp.txt', 'a+'))
+
+            l_win.clear()
+            l_win.addstr(0,0, output)
+            r_win.clear()
+            r_win.addstr(0,0, input_text)
+            b_win.clear()
+            b_win.addstr(0,0, "Input num={:02d}".format(num))
+
+            #show_input(l_win, num)
+            #show_result(r_win, num)
+
+            l_win.refresh()
+            r_win.refresh()
+            b_win.refresh()
+            print("mostro testo", file=open('tmp.txt', 'a+'))
+            print("attendo", file=open('tmp.txt', 'a+'))
+            c = b_win.getch()
+            print("ho atteso", file=open('tmp.txt', 'a+'))
+
+    #except:
+    #    curses.nocbreak()
+    #    stdscr.keypad(False)
+    #    curses.echo()
+    #    curses.endwin()
+    #    print("Unspecified Error")
+
+    finally:
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+
+
+if __name__ == '__main__':
+    try:
+        wrapper(main)
+    except:
+        print("Prova ad ingrandire il terminale...")
---
+++ minizinc/tmp_on_solvers/plot_solver_times.py
@@ -0,0 +1,230 @@
+# TODO
+# Plotta informazioni sui solveTime al variare dei solver
+
+import my_lib
+import os
+import sys
+import json
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-deep')
+
+
+
+keys = ['M','P','O','Q']
+
+def get_all_data(solver_dir, not_filter=False):
+    """ Raccolgo i dati che devo plottare """
+    if not solver_dir[-1] == '/':
+        solver_dir += '/'
+
+    inputs_dir = solver_dir + 'inputs/'
+    outputs_dir = solver_dir + 'outputs/'
+
+
+    inputs = []
+    solveTimes = []
+    nums = []
+
+    num = 0
+    inp = my_lib.get_input(num, inputs_dir)
+    out = my_lib.read_output(num, outputs_dir, suppress_error=True)
+    while not (out == None):
+        if (inp['K'] == K and inp['H'] == H) or not_filter:
+            inputs.append(inp)
+            solveTimes.append(out['stats']['solveTime'])
+            nums.append(num)
+
+        num+=1
+        inp = my_lib.get_input(num, inputs_dir)
+        out = my_lib.read_output(num, outputs_dir, suppress_error=True)
+    return inputs, solveTimes, nums
+
+
+
+
+def sort_by_solveTime(s_inputs, s_solveTimes, s_nums):
+    """ Ordino per solveTime crescente """
+    s_inputs = [y for x,y in sorted(zip(s_solveTimes, s_inputs))]
+    s_nums   = [y for x,y in sorted(zip(s_solveTimes, s_nums))]
+    s_solveTimes = list(sorted(s_solveTimes))
+    return s_inputs, s_solveTimes, s_nums
+
+
+def select_to_plot(inputs, solveTimes, nums):
+    """
+    Selezioni le istanze che poi plottero'
+    Mantengo gli indici relativi
+    """
+    ind_min_sTime = None # risolto in meno tempo
+    ind_max_sTime = None # risolto in piu' tempo
+    ind_min_peps  = None  # con minor numero di persone
+    ind_max_peps  = None  # con maggior numero di persone
+    for i, inp in enumerate(inputs):
+        if ind_min_sTime == None or solveTimes[i] < solveTimes[ind_min_sTime]:
+            ind_min_sTime = i
+        elif ind_max_sTime == None or solveTimes[i] > solveTimes[ind_max_sTime]:
+            ind_max_sTime = i
+
+        if ind_min_peps == None and ind_max_peps == None:
+            ind_min_peps = i
+            ind_max_peps = i
+
+        m_peps = 0 # min num persone
+        M_peps = 0 # max num persone
+        peps = 0   # persone correnti
+        for k in keys:
+            m_peps += inputs[ind_min_peps][k]
+            M_peps += inputs[ind_max_peps][k]
+            peps += inp[k]
+        if peps < m_peps:
+            ind_min_peps = i
+        elif peps > M_peps:
+            ind_max_peps = i
+
+
+    # Rimuovo duplicati
+    selected = list(set([ind_min_sTime, ind_max_sTime, ind_min_peps, ind_max_peps]))
+    s_inputs     = [inputs[i] for i in selected]
+    s_solveTimes = [solveTimes[i] for i in selected]
+    s_nums       = [nums[i] for i in selected]
+
+    return s_inputs, s_solveTimes, s_nums
+
+
+def autolabel(rects, ax):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+def double_plot(s_inputs, s_nums, s_solveTimes1, s_solveTimes2, show=False, save=True, solvers_labels=[""]):
+    m = [i['M'] for i in s_inputs]
+    p = [i['P'] for i in s_inputs]
+    o = [i['O'] for i in s_inputs]
+    q = [i['Q'] for i in s_inputs]
+
+    labels = [str(n) for n in s_nums]
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.1  # the width of the bars
+
+    fig, ax1 = plt.subplots()
+
+    # Plotto tempi
+    times1 = ax1.plot(x, s_solveTimes1, color='red')
+    times2 = ax1.plot(x, s_solveTimes2, color='blue')
+    # End # Plotto tempi
+
+    ## Plotto num persone
+    #ax2 = ax1.twinx()
+    #n_pers = [i['M']+i['P']+i['O']+i['Q'] for i in s_inputs]
+    #pers = ax2.plot(x, n_pers, color='orange', alpha=0.7)
+    ## End Plotto num persone
+
+
+    # Plotto barre
+    ax = ax1.twinx() # instantiate a second axes that shares the same x-axis
+
+    rects_m = ax.bar(x - 3*width/2, m, width, label='M')
+    rects_p = ax.bar(x - width/2, p, width, label='P')
+    rects_o = ax.bar(x + width/2, o, width, label='O')
+    rects_q = ax.bar(x + 3*width/2, q, width, label='Q')
+    #for pos in x:
+    #    ax.annotate('{:.2f}'.format(s_solveTimes[pos]),
+    #                xy=(pos, 0),
+    #                xytext=(0, 3),  # 3 points vertical offset
+    #                textcoords="offset points",
+    #                #TODO background color?
+    #                ha='center', va='bottom')
+    # End # Plotto barre
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    #ax.set_ylabel('Values')
+    ax.set_xlabel('Input Num')
+    ax.set_title('K={} H={}'.format(K,H))
+    #ax.set_yticks(range(max(m+p+o+q)+1))
+    ax.set_yticks([])
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    lns = [rects_m, rects_p, rects_o, rects_q, times1[0], times2[0]]
+    labs = [l.get_label() for l in lns]
+    labs = labs[:-2] + ['solveTimes '+solvers_labels[0], 'solveTimes2 '+solvers_labels[1]]
+    ax.legend(lns, labs)
+
+    autolabel(rects_m,ax)
+    autolabel(rects_p,ax)
+    autolabel(rects_o,ax)
+    autolabel(rects_q,ax)
+
+
+
+    fig.tight_layout()
+    if save:
+        fig_path = 'fig_K{}H{}.png'.format(K,H)
+        fig.savefig(fig_path)
+
+    if show:
+        plt.show()
+
+
+K = 3
+H = 2
+def main():
+    #args = sys.argv
+    #if len(args) > 2:
+    #    #print("python output_visualizer.py arr_outputs 18")
+    #    print("Al piu' un argomento")
+    #    exit(1)
+
+    #try:
+    #    num = int(num)
+    #except:
+    #    print("ERROR!! %s must be a number!!" %(num))
+    #    exit(1)
+    plot_all = True
+    sort     = True
+
+    show     = True
+    save     = True
+
+    gecode_dir = './gecode/'
+    chuffed_dir = './chuffed/'
+
+    g_inputs, g_solveTimes, g_nums = get_all_data(gecode_dir , not_filter=True)
+    c_inputs, c_solveTimes, c_nums = get_all_data(chuffed_dir, not_filter=True)
+
+    cut_len = min(len(g_inputs), len(c_inputs))
+
+    g_inputs, g_solveTimes, g_nums = g_inputs[:cut_len], g_solveTimes[:cut_len], g_nums[:cut_len]
+    c_inputs, c_solveTimes, c_nums = c_inputs[:cut_len], c_solveTimes[:cut_len], c_nums[:cut_len]
+
+
+    #if plot_all:
+    #    s_inputs, s_solveTimes, s_nums = inputs, solveTimes, nums
+    #else:
+    #    s_inputs, s_solveTimes, s_nums = select_to_plot(inputs, solveTimes, nums)
+
+    #if sort:
+    #    s_inputs, s_solveTimes, s_nums = sort_by_solveTime(s_inputs, s_solveTimes, s_nums)
+
+    inputs = g_inputs
+    nums   = g_nums
+    solveTimes1 = g_solveTimes
+    solveTimes2 = c_solveTimes
+    solvers_labels = ['gecode', 'chuffed']
+
+    double_plot(inputs, nums, solveTimes1, solveTimes2, show, save, solvers_labels)
+
+
+
+if __name__ == '__main__':
+    main()
