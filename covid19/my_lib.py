96b33c6f77b13c349e52c37e643b02a06a519a98
Nyriu <muninitristano@gmail.com>
2020-04-30 23:08:33 +0200

    Big refactoring

--- covid19/my_lib.py
+++ covid19/my_lib.py
@@ -23,6 +23,7 @@ class IOHelper:
     """ Classe statica ausiliaria per gestire meglio la generazione/distruzione
         di cartelle o file """
 
+    # On strings ####################
     def correct_dir_path(dir_path):
         """ Aggiunge in fondo a dir_path se necessario """
         if not dir_path[-1] == '/':
@@ -39,11 +40,18 @@ class IOHelper:
         fpath += suffix
         return fpath
 
+
+    # On files ####################
     def open_file(fpath, mode='r'):
         # TODO try catch
         f = open(fpath, mode)
         return f
 
+    # On dirs ####################
+    def create_dir(target_dir):
+        os.makedirs(target_dir, exist_ok=True)
+    def del_dir(target_dir):
+        shutil.rmtree(target_dir, ignore_errors=True)
 
 
 class MyInstance:
@@ -100,20 +108,20 @@ class MyInstance:
     def get_corridoi(self):
         assert(self.values['K']>0)
         return self.values['K']
-    def get_stanze_per_lato(self,h):
+    def get_stanze_per_lato(self):
         assert(self.values['H']>0)
         return self.values['H']
     def get_malati(self):
-        assert(self.values['M']>0)
+        assert(self.values['M']>=0)
         return self.values['M']
     def get_positivi(self):
-        assert(self.values['P']>0)
+        assert(self.values['P']>=0)
         return self.values['P']
     def get_osservazione(self):
-        assert(self.values['O']>0)
+        assert(self.values['O']>=0)
         return self.values['O']
     def get_quarantena(self):
-        assert(self.values['Q']>0)
+        assert(self.values['Q']>=0)
         return self.values['Q']
 
     def set_corridoi(self,k):
@@ -138,12 +146,12 @@ class MyInstance:
     # Metodi statici ####################
     def read(fpath):
         """ Ritorna MyIstance leggendo il file al path indicato """
-         # TODO qua gestire in automatico i due formati
-
+        #TODO qua gestire in automatico i due formati
+        #TODO warning se e' mal formato
         if not os.path.isfile(fpath):
             return None
 
-        f = IOHelper.open_file(fpath, 'w+')
+        f = IOHelper.open_file(fpath, 'r')
 
         comment_char = '%'
         values = {'K':None,'H':None,'M':None,'P':None,'O':None,'Q':None}
@@ -163,11 +171,6 @@ class MyInstance:
                 if k in values.keys():
                     # TODO try catch su int
                     values[k] = int(v)
-
-        for k in values.keys():
-            if values[k] == -1:
-                print("Not well formatted input")
-                exit(2)
 
         ins = MyInstance( k=values['K'], h=values['H'], m=values['M'],
                           p=values['P'], o=values['O'], q=values['Q'])
@@ -233,13 +236,105 @@ class MySolution():
 
 class InputGenerator:
 
-    def satura_stanze(MyInstance):
-        """ Data una MyInstance torna una MyInstance in cui i valori
-            degli ospiti sono stati modificati per occupare tutte le stanze """
+    def satura_stanze(ins):
+        """ Modifica lo stato della MyInstance aggiungendo casualmente persone
+            fino a riempire tutte le stanze disponibili """
+        assert(ins.stanze_necessarie() <= ins.numero_stanze())
 
-    def istanza_casuale(k=None,h=None):
-        """ Se k e h non vengono specificati torna una MyInstance casuale
-            Altrimenti torna un'istanza casuale per k e h dati """
+        getters = [
+                ins.get_malati,
+                ins.get_positivi,
+                ins.get_osservazione,
+                ins.get_quarantena
+                ]
+        setters = [
+                ins.set_malati,
+                ins.set_positivi,
+                ins.set_osservazione,
+                ins.set_quarantena
+                ]
+        while ins.stanze_necessarie() <= ins.numero_stanze():
+            num = randint(0,len(getters)-1)
+            g,s = zip(getters,setters)[num]
+            val = g()
+            s(val+1)
+            last_num = num
+
+        val = getters[last_num]()
+        setters[last_num](val-1)
+        # Se ho valori dispari significa che ho una sola persona
+        # dove potrebbero essercene due (da escludere quelli in osservazione)
+        getters.remove(ins.get_osservazione)
+        setters.remove(ins.set_osservazione)
+        for g,s in zip(getters,setters):
+            val = g()
+            if val%2 == 1:
+                s(val+1)
+
+        assert(ins.stanze_necessarie() <= ins.numero_stanze())
+
+
+    def istanza_casuale(k,h):
+        """ Torna un'istanza casuale per k e h dati """
+        ins = MyInstance(k,h).satura_stanze()
+        getters = [
+                ins.get_malati,
+                ins.get_positivi,
+                ins.get_osservazione,
+                ins.get_quarantena
+                ]
+        setters = [
+                ins.set_malati,
+                ins.set_positivi,
+                ins.set_osservazione,
+                ins.set_quarantena
+                ]
+
+        if randint(0,2) != 2: # due volte su tre
+            # rimuovo un po' di persone a caso
+            n_rimuovere = randint(1, capienza_max(values)//4)
+            rimosse=0
+            while rimosse != n_rimuovere:
+                num = randint(0,len(getters)-1)
+                da_rimuovere = randint(1,n_rimuovere-rimosse)
+                g,s = zip(getters,setters)[num]
+                val = g()
+                if val >= da_rimuovere:
+                    s(val-da_rimuovere)
+                    rimosse += da_rimuovere
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+        else:
+            ## dimezzo Malati, Positivi o Quarantena
+            #getters.remove(ins.get_osservazione)
+            #setters.remove(ins.set_osservazione)
+            #keys = ['M','P','Q']
+            #key = keys[randint(0,len(keys)-1)]
+            #values[key] = values[key]//2 + values[key]%2 #mantengo dispari se era disp
+            print("else")
+
+
+        ## Se nessun valore e' > 0 ... qualcosa non va
+        #qulcs_non_va = True
+        #for key in keys:
+        #    qulcs_non_va = qulcs_non_va and (values[key] == 0)
+        #if qulcs_non_va:
+        #    print("ERROR! Qualcosa non va... Values tutti == 0")
+
+        ## Verifico che ci sia almeno un malato
+        ## Se non c'e' allora converto uno degli altri in malato
+        #if values['M'] == 0:
+        #    key = keys[randint(0,len(keys)-1)]
+        #    while not values[key] > 0:
+        #        key = keys[randint(0,len(keys)-1)]
+        #    if values[key] > 0:
+        #        values[key] -=1
+        #        values['M'] +=1
+        #return values
 
     def write_istanze(n, k_min,h_min, k_max,h_max,
             order=None, delete_old=True):
@@ -253,8 +348,6 @@ class InputGenerator:
         """ Torna una lista di n istanze casuali con
             k in range(k_min,k_max) e h in range(h_min,h_max) """
 
-    def _del_inputs(input_dir = INPUT_DIR):
-        shutil.rmtree(input_dir, ignore_errors=True)
 
 
 
